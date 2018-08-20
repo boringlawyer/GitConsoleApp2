322f1b296387ff4432c3787ec0af3a4c2ffb64b9
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2018-08-20 19:02:56 -0400

    deleted unnecessary code
    Deleted commented out code and unused functions

--- GitConsoleApp/tac.h
+++ GitConsoleApp/tac.h
@@ -3,14 +3,7 @@
 #include "TacObject.h"
 #include "TacCommit.h"
 #include <map>
-bool AddTacCommit(vector<TacCommit*> &commits, TacCommit* newTacCommit);
-void AttachObjects(vector<TacCommit*> &objects, TacCommit* child, int yParam);
-TacCommit* FindExistingTacCommit(vector<TacCommit*> objects, git_commit* current);
 void BuildTacTree(/*vector<TacCommit*> &objects, git_commit* current*/);
 void BuildChild_Parents(git_commit* branchCommit);
 void BuildGit_Tac();
 extern int placeY;
-// maps a commit to its parents
-//extern map<git_commit*, vector<git_commit*>> child_Parents;
-// maps the git_commit to the derived TacCommit
-//extern map<git_commit*, TacCommit*> git_Tac;
